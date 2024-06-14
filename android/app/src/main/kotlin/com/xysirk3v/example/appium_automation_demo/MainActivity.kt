eeb22765523b80ba6601d4a62cea68f2d18730f0
kevin <kevinoduor8877@gmail.com>
2024-06-14 10:23:49 +0300

    MainActivity.kt

--- android/app/src/main/kotlin/com/xysirk3v/example/appium_automation_demo/MainActivity.kt
+++ android/app/src/main/kotlin/com/xysirk3v/example/appium_automation_demo/MainActivity.kt
@@ -2,4 +2,15 @@ package com.xysirk3v.example.appium_automation_demo
 
 import io.flutter.embedding.android.FlutterActivity
 
+@BeforeClass
+@Throws(MalformedURLException::class)
+fun setUp() {
+    val caps: DesiredCapabilities = DesiredCapabilities()
+    caps.setCapability("automationName", "UiAutomator1")
+    caps.setCapability("platformName", "Android")
+    caps.setCapability("platformVersion", "7.1.1")
+    caps.setCapability("deviceName", "Android Emulator")
+    caps.setCapability("app", System.getProperty("user.dir") + "/apps/selendroid-test-app-0.17.0.apk")
+    driver = AndroidDriver(URL("http://localhost:4723/wd/hub"), caps)
+}
 class MainActivity: FlutterActivity()
--- scripts/emulator-up.sh
+++ scripts/emulator-up.sh
@@ -1,7 +1,4 @@
-#https://docs.travis-ci.com/user/languages/android/
-#https://developer.android.com/training/testing/continuous-integration#:~:text=The%20CI%20system%20detects%20a,or%20emulator%20images%20if%20needed.
 #!/bin/bash
-
 set -e #stop immediately if any error happens
 
 avd_name=$1
@@ -13,7 +10,7 @@ fi
 #check if emulator works well
 emulator -version
 
-# create virtual device, default using Android 9 Pie image (API Level 28)
+# create virtual device, default using Android 10 image (API Level 29)
 echo no | avdmanager create avd -n avd28 -k "system-images;android-29;google_apis;x86_64"
 
 # start the emulator
