2e49d1b386ae706bd69ad420d41151eff8e7e2fa
kevin <kevinoduor8877@gmail.com>
2024-06-20 16:35:45 +0300

    Implement Appium Integration with Android 10 and Allure Reporter

--- wdio.conf.js
+++ wdio.conf.js
@@ -138,7 +138,11 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec',['allure', {outputDir: 'allure-results'}]],
+    reporters: ['spec',['allure', {
+        outputDir: 'allure-results',
+        disableWebdriverStepsReporting: true,
+        disbleWebdriverScreenShotsReporting: true,
+    }]],
 
     // Options to be passed to Mocha.
     // See the full list at http://mochajs.org/
